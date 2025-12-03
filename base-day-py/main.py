f9c64770370c589bba6ed19e8cf45ff9b0bf3c45
JosCla <jozerclaver@gmail.com>
2025-12-03 06:14:00 -0700

    Initial commit

---
+++ base-day-py/main.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import sys
+lines = []
+for line in sys.stdin:
+    lines.append(line[:-1])
+
+def partOne(lines):
+    return 0
+
+def partTwo(lines):
+    return 1
+
+print("Part One:", partOne(lines))
+print("Part Two:", partTwo(lines))
